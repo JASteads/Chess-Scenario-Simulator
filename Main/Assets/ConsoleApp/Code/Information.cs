617391d69efb2318db9b4babf8ac02f30c8636b4
Milton Pope <jfi15@txstate.edu>
2024-07-17 10:49:52 -0500

    Update Information.cs

--- Main/Assets/ConsoleApp/Code/Information.cs
+++ Main/Assets/ConsoleApp/Code/Information.cs
@@ -22,8 +22,18 @@ namespace ConsoleApp.Code
 
         }
 
-
-
+        /* int[] chessPiece = new [] {"Pawn", "Rook", "Knight", "Bishop", "Queen", "King"};
+        int[] color = new[] {"White", "Black"};
+        int[] chessBoard = new[] {"A1" , "B1", "C1", "D1", "E1", "F1", "G1", "H1",
+                                  "A2" , "B2", "C2", "D2", "E2", "F2", "G2", "H2",
+                                  "A3" , "B3", "C3", "D3", "E3", "F3", "G3", "H3",
+                                  "A4" , "B4", "C4", "D4", "E4", "F4", "G4", "H4",
+                                  "A5" , "B5", "C5", "D5", "E5", "F5", "G5", "H5",
+                                  "A6" , "B6", "C6", "D6", "E6", "F6", "G6", "H6",
+                                  "A7" , "B7", "C7", "D7", "E7", "F7", "G7", "H7",
+                                  "A8" , "B8", "C8", "D8", "E8", "F8", "G8", "H8",
+                                 };
+        */
 
         public short getPosition()
         {
@@ -60,11 +70,12 @@ namespace ConsoleApp.Code
             teamColor = teamColor >> 9;
         }
 
-        public void printInformation()
+        /*public void printInformation()
         {
-            ConsoleApp.WriteLine("Position: ")
-            ConsoleApp.WriteLine("Piece Type: ")
-            ConsoleApp.WriteLine("Team Color: ")
+            ConsoleApp.WriteLine("Position: " + chessBoard[position]);
+            ConsoleApp.WriteLine("Piece Type: " + chessPiece[pieceType]);
+            ConsoleApp.WriteLine("Team Color: " + color[teamColor]);
         }
+        */
     }
 }
