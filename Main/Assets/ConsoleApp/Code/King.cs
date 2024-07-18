f2cfbd32b385e1f1d6018ff9b1372bc54e8afbb8
Milton Pope <jfi15@txstate.edu>
2024-07-17 23:43:26 -0500

    Multiple Edits to multiple classes

--- Main/Assets/ConsoleApp/Code/King.cs
+++ Main/Assets/ConsoleApp/Code/King.cs
@@ -1,5 +1,17 @@
 public class King:Piece
 {
+
+
+    public King(ushort location, ushort team) : base(location, team)
+    {
+        SetType(5);
+    }
+    public override bool CheckMoves()
+    {
+        return true;
+    }
+
+
     public bool HasMoved;
     public bool IsChecked;
 
