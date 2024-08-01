182f11e92f5fe3a7b81bda7fa653a5b58236ae59
HAK1095 <hammadkhan_95@outlook.com>
2024-07-31 22:19:44 -0700

    Add files via upload

---
+++ PuzzleGenerator.tests/PuzzleGeneratorTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+public class PuzzleGeneratorTests
+{
+    private PuzzleGenerator _puzzleGenerator;
+
+    public void Setup()
+    {
+        _puzzleGenerator = new PuzzleGenerator
+        {
+            Simulation = new Game(),
+            TargetRating = 100,
+            NumAttempts = 3,
+            ScenarioList = new List<Scenario>(),
+            Score = 0
+        };
+    }
+
+    [TestMethod]
+    public void GeneratePuzzle_ShouldReturnNull()
+    {
+        var result = _puzzleGenerator.GeneratePuzzle();
+        Assert.IsNull(result);
+    }
+
+    [TestMethod]
+    public void StartPuzzle_ShouldInitializePuzzle()
+    {
+        _puzzleGenerator.StartPuzzle();
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void Update_ShouldUpdateGameState()
+    {
+        _puzzleGenerator.Update();
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void AddNoise_ShouldAddNoiseToScenario()
+    {
+        _puzzleGenerator.AddNoise();
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void OnMove_ShouldProcessMove()
+    {
+        _puzzleGenerator.OnMove();
+        Assert.IsTrue(true);
+    }
+}
